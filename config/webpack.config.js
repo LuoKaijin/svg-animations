a8d2da82c22a4067def3fc1c7eae2880dd08a8df
rainnaZR <denzu111@163.com>
2017-04-27 09:49:52 +0800

    MISC: 文件名修改

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
         index: path.resolve(__dirname, '../src/index.js'),
         volumn: path.resolve(__dirname, '../src/pages/step2/volumn/index.js'),
         path: path.resolve(__dirname, '../src/pages/step2/path/index.js'),
-        letterChange: path.resolve(__dirname, '../src/pages/step2/letterChange/index.js')
+        letterChange: path.resolve(__dirname, '../src/pages/step2/letter.change/index.js')
     },
     output: {
         filename: '[name].bundle.js',
